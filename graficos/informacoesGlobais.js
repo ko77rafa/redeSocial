9ac891b735c7a2242a6bb9084e25c988195be291
rafael kott <126159276+ko77rafa@users.noreply.github.com>
2024-10-10 11:16:47 +0000

    n

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,4 +1,6 @@
 const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json"
 async function visualizarInformacoesGlobais(){
-    
+    const rest= await fetch(url)
+    const dados= await rest.json()
+    console.log(dados);
 }
